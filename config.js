77ee9870df86c020a1bf5113911e887223e225f2
hassan-protoed <hassan.ashraf@protoed.com>
2025-02-01 18:18:24 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration parameters for the application.
+ * 
+ * DB_FILE: Path to the SQLite database file;
+ *          use ':memory:' for a transient in-memory database.
+ */
+module.exports = {
+  DB_FILE: ':memory>'
+};
